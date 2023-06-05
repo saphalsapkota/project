b3d04265b18838b8de8d923844f107fca074a038
saphal <saphalsapkota96@gmail.com>
2023-06-05 18:43:40 +0545

    login

---
+++ login.js
@@ -0,0 +1,17 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    // Perform login validation and AJAX request here
+    var username = document.getElementsByName("username")[0].value;
+    var password = document.getElementsByName("password")[0].value;
+  
+    // Example login validation
+    if (username === "admin" && password === "password") {
+      // Successful login, redirect or perform necessary actions
+      console.log("Login successful");
+    } else {
+      // Display error message
+      var errorDiv = document.getElementById("loginError");
+      errorDiv.textContent = "Invalid username or password.";
+    }
+  });
